7c540b70cdaa185b69f427206c960076d7a837ce
TerangaHomes Developer <developer@terangahomes.com>
2026-03-21 00:28:25 +0000

    Nouvel accueil inspiré de Seloger.com + Booking.com - Design moderne et professionnel

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-// Rediriger vers l'accueil bleu simple et détruire les sessions
+// Rediriger vers l'accueil Seloger + Booking et détruire les sessions
 
 session_start();
 session_destroy();
 
-header('Location: accueil_bleu_simple.php');
+header('Location: accueil_seloger_booking.php');
 exit;
 ?>
